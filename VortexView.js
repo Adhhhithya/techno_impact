72d1ba313398d35427d0c68c39bb4de3bfcb7e92
Israel Paul <130955174+I-paul@users.noreply.github.com>
2024-11-06 00:45:46 +0530

    initial website prototype

---
+++ VortexView.js
@@ -0,0 +1,16 @@
+function checkSizeAndOpenResultPage() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const fileSizeInMB = file.size / (1024 * 1024);
+        if (fileSizeInMB > 20) {
+            alert("File size exceeds 20 MB limit. Please upload a smaller file.");
+            return;
+        }
+
+        window.location.href = "Resultpage.html";
+    } else {
+        alert("Please choose a file first.");
+    }
+}
